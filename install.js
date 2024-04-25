9a543185496aeb3d21ae343f6c69b6af72e87f00
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-25 10:21:26 -0400

    init

---
+++ install.js
@@ -0,0 +1,63 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://huggingface.co/spaces/cocktailpeanut/ov2 app",
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        path: "app",
+        message: [
+          "git clone --depth 1 https://huggingface.co/cocktailpeanut/ov2 checkpoints"
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "{{platform === 'darwin' ? 'brew install mecab' : null}}",
+          "pip install gradio==3.50.2 devicetorch",
+          "pip install git+https://github.com/myshell-ai/OpenVoice.git",
+          "pip install git+https://github.com/peanutcocktail/MeloTTS.git",
+          "python -m unidic download",
+          "pip install whisper-timestamped==1.15.4",
+        ]
+      }
+    },
+    //  Uncomment this step to add automatic venv deduplication (Experimental)
+    //  {
+    //    method: "fs.link",
+    //    params: {
+    //      venv: "app/env"
+    //    }
+    //  },
+    {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!"
+      }
+    }
+  ]
+}
---
+++ pinokio.js
@@ -0,0 +1,114 @@
+const path = require('path')
+module.exports = {
+  version: "1.5",
+  title: "Openvoice2",
+  description: "",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "app", "env")
+    let running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "English",
+          href: "start.js",
+          params: {
+            install: "pip uninstall -y mecab-python3 python-mecab-ko && pip install -U mecab-python3==1.0.5",
+            run: "python app.py EN"
+          }
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "Spanish",
+          href: "start.js",
+          params: {
+            install: "pip uninstall -y mecab-python3 python-mecab-ko && pip install -U mecab-python3==1.0.5",
+            run: "python app.py ES"
+          }
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "French",
+          href: "start.js",
+          params: {
+            install: "pip uninstall -y mecab-python3 python-mecab-ko && pip install -U mecab-python3==1.0.5",
+            run: "python app.py FR"
+          }
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "Chinese",
+          href: "start.js",
+          params: {
+            install: "pip uninstall -y mecab-python3 python-mecab-ko && pip install -U mecab-python3==1.0.5",
+            run: "python app.py ZH"
+          }
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "Korean",
+          href: "start.js",
+          params: {
+            install: (() => {
+              if (kernel.platform === "win32") {
+                return "pip uninstall -y mecab-python3 && pip install -U eunjeon"
+              } else {
+                return "pip uninstall -y mecab-python3 && pip install -U python-mecab-ko"
+              }
+            })(),
+            run: "python app.py KR"
+          }
+        }, {
+          icon: "fa-solid fa-power-off",
+          text: "Japanese",
+          href: "start.js",
+          params: {
+            install: "pip uninstall -y mecab-python3 python-mecab-ko && pip install -U mecab-python3==1.0.5",
+            run: "python app.py JP"
+          }
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
+}
