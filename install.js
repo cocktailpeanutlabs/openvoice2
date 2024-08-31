90352c99381b0e01073db9c753bd8994b2e9f864
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-30 20:03:56 -0400

    update

--- install.js
+++ install.js
@@ -48,11 +48,11 @@ module.exports = {
         message: [
           "{{platform === 'darwin' ? 'brew install mecab' : null}}",
           "pip install gradio==3.50.2 devicetorch",
-          //"pip install git+https://github.com/myshell-ai/OpenVoice.git",
-          "pip install git+https://github.com/peanutcocktail/OpenVoice.git",
+          "pip install git+https://github.com/myshell-ai/OpenVoice.git",
+          //"pip install git+https://github.com/peanutcocktail/OpenVoice.git",
           "pip install git+https://github.com/peanutcocktail/MeloTTS.git",
           "python -m unidic download",
-          "pip install whisper-timestamped==1.15.4",
+          //"pip install whisper-timestamped==1.15.4",
         ]
       }
     },
